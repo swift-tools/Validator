5aec8dccdf31051b515566ad44a7a171f9fd80fa
lazymisu <felix.chacaltana.felix@gmail.com>
2023-05-11 20:15:02 -0500

    Improve `validate()` now returns a Boolean

--- Validator/Classes/Validator.swift
+++ Validator/Classes/Validator.swift
@@ -10,7 +10,7 @@ import Foundation
 public protocol Validation {
     var errorKey: String { get }
     var errorMessage: String { get }
-    func validate(_ field: String) -> ValidationResult
+    func validate(_ field: String) -> Bool
 }
 
 public struct ValidationResult {
@@ -46,47 +46,38 @@ public final class Validator {
     
     public func validate(_ field: String) -> ValidationResult {
         for validation in validations {
-            let result = validation.validate(field)
-            if !result.isValid, let error = result.error {
-                let errorMessage = self.customErrorMessages[error.errorKey] ?? error.errorMessage
-                return ValidationResult(isValid: false, error: ValidationError(errorMessage: errorMessage, errorKey: error.errorKey))
+            let isValid = validation.validate(field)
+            if !isValid {
+                let errorMessage = self.customErrorMessages[validation.errorKey] ?? validation.errorMessage
+                let validationError = ValidationError(errorMessage: errorMessage, errorKey: validation.errorKey)
+                return ValidationResult(isValid: isValid, error: validationError)
             }
         }
         return ValidationResult(isValid: true, error: nil)
     }
 }
 
-public final class RequiredValidation: Validation {
-    public var errorKey: String {
-        return "Required"
-    }
-    
-    public var errorMessage: String {
-        return "This field is required."
-    }
-    
+public struct RequiredValidation: Validation {
     public init() { }
     
-    public func validate(_ field: String) -> ValidationResult {
-        let isValid = !field.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-        return ValidationResult(isValid: isValid, error: ValidationError(errorMessage: errorMessage, errorKey: errorKey))
+    public var errorKey: String { "Required" }
+    
+    public var errorMessage: String { "This field is required." }
+    
+    public func validate(_ field: String) -> Bool {
+        return !field.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
     }
 }
 
-public final class EmailValidation: Validation {
-    public var errorKey: String {
-        return "Email"
-    }
-    
-    public var errorMessage: String {
-        return "Please enter a valid email address."
-    }
-    
+public struct EmailValidation: Validation {
     public init() { }
     
-    public func validate(_ field: String) -> ValidationResult {
+    public var errorKey: String { "Email" }
+
+    public var errorMessage: String { "Please enter a valid email address." }
+
+    public func validate(_ field: String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let isValid = NSPredicate(format:"SELF MATCHES %@", emailRegex).evaluate(with: field)
-        return ValidationResult(isValid: isValid, error: ValidationError(errorMessage: errorMessage, errorKey: errorKey))
+        return NSPredicate(format:"SELF MATCHES %@", emailRegex).evaluate(with: field)
     }
 }
